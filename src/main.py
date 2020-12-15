1ce8a9dce779f784001a83e2ceb4150b8e3806e4
Weiliang Xie <weiliangxie@hotmail.com>
2020-12-15 16:11:49 -0500

    typo

--- src/main.py
+++ src/main.py
@@ -4,31 +4,7 @@ from NB_BOW_FV import *
 
 nb_ov = NB_BOW_OV()
 nb_ov.train("./training/covid_training.tsv")
-
-# counterYes = 0
-# counterNo = 0
-
-# for elem in nb_ov.training_tweets.values():
-#     if elem == "yes":
-#         counterYes += 1
-#     else:
-#         counterNo += 1
-
-# print(counterYes, counterNo)
-
 nb_ov.predict("./test/covid_test_public.tsv", "./trace/trace_NB-BOW-OV.txt", "./eval/eval_NB-BOW-OV.txt")
-
-
-# counterYes = 0
-# counterNo = 0
-
-# for elem in nb_ov.test_tweets.values():
-#     if elem[1] == "yes":
-#         counterYes += 1
-#     else:
-#         counterNo += 1
-
-# print(counterYes, counterNo)
 
 
 nb_fv = NB_BOW_FV()
